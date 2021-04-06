87d4388b22161dcb98a76dbabb5ed5aea0cd8ae1
Lee <hangsen.lee@hexainitiative.com>
2021-04-06 15:23:33 +0800

    initial commit

---
+++ src/app/recipe/recipes.page.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { IonicModule } from "@ionic/angular";
+import { RecipesPage } from "../recipes/recipes.page";
+
+describe("RecipesPage", () => {
+  let component: RecipesPage;
+  let fixture: ComponentFixture<RecipesPage>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [RecipesPage],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(RecipesPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
